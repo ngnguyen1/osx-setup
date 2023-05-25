0b0bc6a949cdf50a3aed0a51af3fe7017ae277a2
Nga Nguyen <kyo1508@gmail.com>
2023-05-25 09:08:05 +0700

    using chemacs2 for multple profiles

---
+++ .emacs-profiles.el
@@ -0,0 +1,6 @@
+(
+ ("default" . ((user-emacs-directory . "~/.doom-emacs")
+               (server-name . "doom")))
+ ("legacy"  . ((user-emacs-directory . "~/.emacs.legacy")
+               (server-name . "gnu")))
+)
--- .emacs.el
+++
@@ -1,2 +0,0 @@
-;; load Spacemacs's initialization file, "~" is equivalent to "$HOME"
-(load-file "~/.emacs.d/init.el")
