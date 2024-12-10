9c354c659073df25079a29321b15be2b5940d927
Nga Nguyen <kyo1508@gmail.com>
2024-12-10 22:08:02 +0700

    remove emacs config

--- .emacs-profiles.el
+++
@@ -1,6 +0,0 @@
-(
- ("default" . ((user-emacs-directory . "~/.doom-emacs")
-               (server-name . "doom")))
- ("legacy"  . ((user-emacs-directory . "~/.emacs.legacy")
-               (server-name . "gnu")))
-)
